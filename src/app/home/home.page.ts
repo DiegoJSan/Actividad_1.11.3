7bc9b17bbe159d07b4e20de7390851cec8384409
DiegoJSan <alum.diegojesus.sanchez.delcorral@ieslosremedios.org>
2021-11-10 07:26:33 +0100

    Commit inicial

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { HttpClient} from '@angular/common/http';
 
 @Component({
   selector: 'app-home',
@@ -7,6 +8,16 @@ import { Component } from '@angular/core';
 })
 export class HomePage {
 
-  constructor() {}
+
+  usuarios: any;
+  filtro: string = '';
+
+  constructor(private httpClient: HttpClient) {
+
+    this.usuarios = this.httpClient.get('https://raw.githubusercontent.com/DiegoJSan/Actividad_1.11.2/master/MOCK_DATA.json')
+
+  }
+
+  
 
 }
